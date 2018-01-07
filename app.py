5bbefe78e5f5115c9538e5fba968bb8dd0386bf4
Badibanga <cba50@ostml0244737.home>
2018-01-07 18:09:46 +0000

    returning simple html template

--- app.py
+++ app.py
@@ -1,13 +1,10 @@
 import os
-from flask import Flask
-from redis import Redis
+from flask import Flask, render_template
 app = Flask(__name__)
-
-redis = Redis(host="redis", db=0, socket_connect_timeout=2, socket_timeout=2)
 
 @app.route('/')
 def hello_world():
-    return 'Hello World. We\'re finally doing this'
+    return render_template('index.html')
 
 if __name__ == '__main__':
     app.run(debug=True, host="0.0.0.0", port=80)
