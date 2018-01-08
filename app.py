4af1d98b37096054a471d476dd69a1e20d70b12c
mscbee <charlenebadibanga@gmail.com>
2018-01-08 22:40:34 +0000

    continued on authorisation process with correct redirection to /callback/

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ import os
 from flask import Flask, render_template, redirect, url_for, request
 import requests
 import base64
-import urllib
+from urllib.parse import quote
 import string
 from random import *
 
@@ -15,27 +15,21 @@ allchar = string.ascii_letters + string.punctuation + string.digits
 
 
 #  Client Keys
-CLIENT_ID = "455caa6c91784978888beca833756069"
-CLIENT_SECRET = "8e4df8b4372f4df6bb80a1dcba10b364"
+CLIENT_ID = <YOUR-CLIENT-ID>
+CLIENT_SECRET = <YOUR-CLIENT-SECRET>
 
 # For authorizsation
-REDIRECT_URI = "http://0.0.0.0:5000/callback/q"
-SCOPE = "playlist-read-private user-library-read user top read"
+REDIRECT_URI = "http://0.0.0.0:5000/callback/"
+SCOPE = "playlist-read-private user-library-read"
 RESPONSE_TYPE = "code"
+# State is not required but highly recommended
 STATE = "".join(choice(allchar) for num in range(randint(2, 6)))
-print(STATE)
-SHOW_DIALOG_bool = False
-# TODO: remember to remove the client ID and secret before pushing
-
-# Spotify URLS
-SPOTIFY_AUTH_URL = "https://accounts.spotify.com/authorize"
-SPOTIFY_TOKEN_URL = "https://accounts.spotify.com/api/token"
-SPOTIFY_API_BASE_URL = "https://api.spotify.com"
-API_VERSION = "v1"
-SPOTIFY_API_URL = "{}/{}".format(SPOTIFY_API_BASE_URL, API_VERSION)
 # This is automatically set to false anyway but I'm including it as a learning exercise
 # It prevents a previously authenticated user from going through the auth process again
 SHOW_DIALOG_bool = False
+# helps with encoding to utf-8 in url_args
+SHOW_DIALOG_str = str(SHOW_DIALOG_bool).lower()
+SPOTIFY_AUTH_URL = "https://accounts.spotify.com/authorize"
 
 
 auth_query_parameters = {
@@ -44,19 +38,20 @@ auth_query_parameters = {
     "redirect_uri": REDIRECT_URI,
     "state": STATE,
     "scope": SCOPE,
-    "show_dialog": SHOW_DIALOG_bool
+    # "show_dialog": SHOW_DIALOG_str
 }
 
-
 @app.route('/')
-def hello_world():
-    return render_template('index.html')
+def index():
+    url_args = "&".join(["{}={}".format(key,quote(val.encode('utf-8'))) for key,val in auth_query_parameters.items()])
+    return redirect("{}/?{}".format(SPOTIFY_AUTH_URL, url_args))
 
-@app.route('/login', methods=['GET'])
-def login():
-    client_id = '455caa6c91784978888beca833756069'
-    return redirect('https://accounts.spotify.com/authorize' + client_id)
-    return render_template('index.html')
+@app.route('/callback/')
+def callback():
+    auth_code = request.args['code']
+
+    return render_template("index.html")
+
 
 
 if __name__ == '__main__':
